8648f8d0f2d3635881a921bf05775cee34559284
LolaInTa Chi <lolainta@gmail.com>
2026-02-06 12:13:42 +0800

    feat: server-wrapper init

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM carlasim/carla:0.9.16
 USER root
-RUN apt update && apt install -y python3-pip curl
+RUN apt update && apt install -y --no-install-recommends git
+COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
 
-USER carla
-ENTRYPOINT [ "./CarlaUE4.sh" ]
-CMD [ "-RenderOffScreen" ]
+WORKDIR /app
+COPY . .
+RUN uv add /workspace/PythonAPI/carla/dist/carla-0.9.16-cp312-cp312-manylinux_2_31_x86_64.whl
+RUN uv add git+https://github.com/lolainta/carla-api.git
--- carla_wrapper/server.py
+++ carla_wrapper/server.py
@@ -2,16 +2,19 @@ import grpc
 from concurrent import futures
 import time
 
-from carla_api import carla_pb2, carla_pb2_grpc
 import carla
+from carla_api import carla_pb2, carla_pb2_grpc
 
 
 class CarlaService(carla_pb2_grpc.CarlaSimServicer):
     def __init__(self):
         print("Connecting to CARLA...")
         self.client = carla.Client("localhost", 2000)
-        self.client.set_timeout(10.0)
+        self.client.set_timeout(30.0)
         self.world = self.client.get_world()
+        settings = self.world.get_settings()
+        settings.no_rendering_mode = True
+        self.world.apply_settings(settings)
         print("Connected to CARLA")
 
     def Ping(self, request, context):
