a996facb36f10de7f93895d6f8b2fcf95aa8d8be
LolaInTa Chi <lolainta@gmail.com>
2026-02-11 21:58:57 +0800

    fix: remove carla server args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd /opt/carla
 echo "Starting CarlaUE4 server..."
-./CarlaUE4.sh -RenderOffScreen -nosound -opengl -carla-port=${CARLA_PORT:-2000} &
+./CarlaUE4.sh -RenderOffScreen -carla-port=${CARLA_PORT:-2000} &
 popd
 sleep 2
 pushd /app
